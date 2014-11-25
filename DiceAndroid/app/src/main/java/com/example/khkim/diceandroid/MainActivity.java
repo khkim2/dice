3caea1f3cc0c92a7a1d0d242a7f6faa0ffc2224c
khkim <khkim@macbookchickenjuicenet.local>
2014-11-26 02:48:22 +0900

    DiceAndroid

---
+++ DiceAndroid/app/src/main/java/com/example/khkim/diceandroid/MainActivity.java
@@ -0,0 +1,208 @@
+package com.example.khkim.diceandroid;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+
+public class MainActivity extends Activity implements SensorEventListener {
+    //private static final String IP = "198.252.206.16";
+    //private static final int PORT = 80;
+    private static final String IP = "169.254.53.22";
+    private static final int PORT = 8080;
+
+    Button btnStart, btnSend;
+    TextView textStatus;
+    NetworkTask networkTask;
+    SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnSend = (Button)findViewById(R.id.btnSend);
+        textStatus = (TextView)findViewById(R.id.textStatus);
+
+        btnStart.setOnClickListener(btnStartListener);
+        btnSend.setOnClickListener(btnSendListener);
+
+        networkTask = new NetworkTask();
+        networkTask.execute();
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private View.OnClickListener btnStartListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            btnStart.setVisibility(View.INVISIBLE);
+            textStatus.setText("Start");
+
+            networkTask = new NetworkTask();
+            networkTask.execute();
+        }
+    };
+
+    private View.OnClickListener btnSendListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            textStatus.setText("Send");
+            //networkTask.SendDataToNetwork("GET / HTTP/1.1\r\n\r\n");
+            networkTask.SendDataToNetwork("1|2|3\n");
+        }
+    };
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        String message = x + "|" + y + "|" + z + "\n";
+        //System.out.print(message);
+        networkTask.SendDataToNetwork(message);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public class NetworkTask extends AsyncTask<Void, byte[], Boolean> {
+        Socket nsocket; //Network Socket
+        InputStream nis; //Network Input Stream
+        OutputStream nos; //Network Output Stream
+
+        @Override
+        protected void onPreExecute() {
+            Log.i("AsyncTask", "onPreExecute");
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) { //This runs on a different thread
+            boolean result = false;
+            try {
+                Log.i("AsyncTask", "doInBackground: Creating socket");
+                SocketAddress sockaddr = new InetSocketAddress(IP, PORT);
+                nsocket = new Socket();
+                nsocket.connect(sockaddr, 5000); //10 second connection timeout
+                if (nsocket.isConnected()) {
+                    nis = nsocket.getInputStream();
+                    nos = nsocket.getOutputStream();
+                    Log.i("AsyncTask", "doInBackground: Socket created, streams assigned");
+                    Log.i("AsyncTask", "doInBackground: Waiting for inital data...");
+                    byte[] buffer = new byte[4096];
+                    int read = nis.read(buffer, 0, 4096); //This is blocking
+                    while(read != -1){
+                        byte[] tempdata = new byte[read];
+                        System.arraycopy(buffer, 0, tempdata, 0, read);
+                        publishProgress(tempdata);
+                        Log.i("AsyncTask", "doInBackground: Got some data");
+                        read = nis.read(buffer, 0, 4096); //This is blocking
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("AsyncTask", "doInBackground: IOException");
+                result = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("AsyncTask", "doInBackground: Exception");
+                result = true;
+            } finally {
+                try {
+                    nis.close();
+                    nos.close();
+                    nsocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Log.i("AsyncTask", "doInBackground: Finished");
+            }
+            return result;
+        }
+
+        public void SendDataToNetwork(String cmd) { //You run this from the main thread.
+            try {
+                if (nsocket.isConnected()) {
+                    //Log.i("AsyncTask", "SendDataToNetwork: Writing received message to socket");
+                    nos.write(cmd.getBytes());
+                } else {
+                    Log.i("AsyncTask", "SendDataToNetwork: Cannot send message. Socket is closed");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("AsyncTask", "SendDataToNetwork: Message send failed. Caught an exception");
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(byte[]... values) {
+            if (values.length > 0) {
+                Log.i("AsyncTask", "onProgressUpdate: " + values[0].length + " bytes received.");
+                textStatus.setText(new String(values[0]));
+            }
+        }
+        @Override
+        protected void onCancelled() {
+            Log.i("AsyncTask", "Cancelled.");
+            btnStart.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Log.i("AsyncTask", "onPostExecute: Completed with an Error.");
+                textStatus.setText("There was a connection error.");
+            } else {
+                Log.i("AsyncTask", "onPostExecute: Completed.");
+            }
+            btnStart.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
